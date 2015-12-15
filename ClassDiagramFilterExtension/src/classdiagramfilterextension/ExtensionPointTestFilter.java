68ddeb068c435d8672a4bd2a0860c76b6170d8c2
jcarias <jalcs@iscte-iul.pt>
2015-12-15 14:19:21 +0000

    Conversão para Interface

--- ClassDiagramFilterExtension/src/classdiagramfilterextension/ExtensionPointTestFilter.java
+++ ClassDiagramFilterExtension/src/classdiagramfilterextension/ExtensionPointTestFilter.java
@@ -7,17 +7,9 @@ import pt.iscde.classdiagram.model.types.EChildElementType;
 import pt.iscde.classdiagram.model.types.EModifierType;
 import pt.iscde.classdiagram.model.types.ETopElementType;
 
-public class ExtensionPointTestFilter extends ClassDiagramFilter {
+public class ExtensionPointTestFilter implements ClassDiagramFilter {
 
-	@Override
-	public String getFilterName() {
-		return "Example filter";
-	}
-
-	@Override
-	public String getFilterShortDescription() {
-		return "Example filter's short description.";
-	}
+	private boolean active;
 
 	@Override
 	public boolean acceptTopElement(ETopElementType type, String name, EModifierType visibility, Set<EModifierType> modifiers) {
@@ -26,6 +18,32 @@ public class ExtensionPointTestFilter extends ClassDiagramFilter {
 
 	@Override
 	public boolean acceptCildElement(EChildElementType type, String name, EModifierType visibility, Set<EModifierType> modifiers, String returnType) {
-		return modifiers.contains(EModifierType.CONSTRUCTOR);
+		return visibility.equals(EModifierType.PUBLIC);
+	}
+
+	@Override
+	public boolean isActive() {
+		return active;
+	}
+
+	@Override
+	public void activate() {
+		this.active = true;
+
+	}
+
+	@Override
+	public void deactivate() {
+		this.active = false;
+	}
+
+	@Override
+	public String getFilterName() {
+		return "Filtro de Teste";
+	}
+
+	@Override
+	public String getFilterShortDescription() {
+		return "Filtro de Teste para exibir apenas classes e os métodos públicos das mesmas";
 	}
 }
