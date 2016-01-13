5d0064bc1f6ffa9d89bfeb12c07fea685b851a4a
jcarias <jalcs@iscte-iul.pt>
2016-01-13 14:51:41 +0000

    Small improvements

--- pa.iscde.classdiagram/src/pt/iscde/classdiagram/extensibility/ClassDiagramStyler.java
+++ pa.iscde.classdiagram/src/pt/iscde/classdiagram/extensibility/ClassDiagramStyler.java
@@ -4,7 +4,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Font;
 
 /**
- * This is the Styler that will allow customization of the Class diagram elements. With an instace
+ * This is the Styler that will allow customization of the Class diagram elements. With an instance
  * of this styler one is able to customize the default appearance of the elements colors and fonts.  
  * 
  * @author joaocarias
@@ -12,8 +12,18 @@ import org.eclipse.swt.graphics.Font;
  */
 public interface ClassDiagramStyler {
 	
+	/**
+	 * Sets the color of the unselected class figure's background 
+	 * @return the unselected background color (not-null)
+	 * 
+	 */
 	public Color getBackgroundColor();
 	
+	/**
+	 * Sets the color of the selected class figure's background 
+	 * @return the selected background color (not-null)
+	 * 
+	 */
 	public Color getSelectedBackgroundColor();
 	
 	public Color getForegroundColor();
